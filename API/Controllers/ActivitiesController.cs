5a04b06328d37db44f89b76270c19349b21f4e3a
Enoque de Andrade Neto <e.de.andrade.neto@accenture.com>
2021-07-10 11:42:09 -0300

    End of 4th Chapter

--- API/Controllers/ActivitiesController.cs
+++ API/Controllers/ActivitiesController.cs
@@ -1,35 +1,49 @@
 using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
+using Application.Activities;
 using Domain;
+using MediatR;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
-using Persistence;
+
 
 namespace API.Controllers
 {
     public class ActivitiesController : BaseAPIController
-    {
-        private readonly DataContext _context;
-        public ActivitiesController(DataContext context)
-        {
-            _context = context;
-        }
-
+    {   
         [HttpGet]
         public async Task<ActionResult<List<Activity>>> GetActivitiesAsync()
         {
-            return await _context.Activities.ToListAsync();
+            return await Mediator.Send(new List.Query());
         }
 
         [HttpGet("{id}")]
-        public async Task<ActionResult<Activity>> GetActivityAsync(Guid Id)
+        public async Task<ActionResult<Activity>> GetActivityAsync(Guid id)
         {
-            var ret = await _context.Activities.FindAsync(Id);
+            var ret = string.Empty;
             if(ret != null)
-                return ret;
+                return await Mediator.Send(new Details.Query{Id = id});
             else
                 return NotFound();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateActivityAsync(Activity activity)
+        {
+            return Ok(await Mediator.Send(new Create.Command{Activity = activity}));
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> EditActivityAsync(Guid id, Activity activity)
+        {
+            activity.Id = id;
+            return Ok(await Mediator.Send(new Edit.Command{Activity = activity}));
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteActivityAsync(Guid id)
+        {
+            return Ok(await Mediator.Send(new Delete.Command{Id = id}));
         }
     }
 }
---
+++ API/Core/MappingProfiles.cs
@@ -0,0 +1,13 @@
+using AutoMapper;
+using Domain;
+
+namespace API.Core
+{
+    public class MappingProfiles : Profile
+    {
+        public MappingProfiles()
+        {
+            CreateMap<Activity,Activity>();
+        }
+    }
+}
