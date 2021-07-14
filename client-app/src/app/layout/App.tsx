6362c49e37dafcda5e570c49182fbefd1614e357
Enoque de Andrade Neto <e.de.andrade.neto@accenture.com>
2021-07-14 12:56:14 -0300

    End of Axios Chapter

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -1,19 +1,28 @@
 import React, { useEffect, useState } from 'react';
-import axios from 'axios';
 import { Container } from 'semantic-ui-react';
 import { Activity } from '../models/activity';
 import NavBar  from '../layout/NavBar'
 import ActivityDashboard from '../../features/activities/dashboard/ActivityDashboard'
 import {v4 as uuid} from 'uuid'
+import agent from '../api/agent';
+import LoadingComponent from '../layout/LoadingComponent';
 
 function App() {
 const [activities, setActivities] = useState<Activity[]>([]);
 const [selectedActivity, setSelectedActivity] = useState<Activity | undefined>(undefined);
 const [editMode, setEditMode] = useState(false);
+const [loading, setLoading] = useState(true);
+const [submitting, setSubmitting] = useState(false);
 
 useEffect(() => {
-  axios.get<Activity[]>('http://localhost:5000/api/Activities').then(response => {
-    setActivities(response.data);
+  agent.ActivityByAxios.list().then(response => {
+    let activities: Activity[] = [];
+    response.forEach(activity =>{
+      activity.date = activity.date.split('T')[0];
+      activities.push(activity);
+      setLoading(false);
+    })
+    setActivities(activities);
   })
 }, [])
 
@@ -35,17 +44,35 @@ function handleFormClose(){
 }
 
 function handleCreateOrEditActivity(activity: Activity) {
-  activity.id 
-  ? setActivities([...activities.filter(x => x.id !== activity.id), activity])
-  : setActivities([...activities, {...activity, id: uuid()}]);
-  setEditMode(false);
-  setSelectedActivity(activity);
+  setSubmitting(true);
+  if (activity.id) {
+    agent.ActivityByAxios.update(activity).then(() => {
+      setActivities([...activities.filter(x => x.id !== activity.id), activity]);
+      setSelectedActivity(activity);
+      setEditMode(false);
+      setSubmitting(false);
+    })
+  } else {
+    activity.id = uuid();
+    agent.ActivityByAxios.create(activity).then(() => {
+      setActivities([...activities, activity]);  
+      setSelectedActivity(activity);
+      setEditMode(false);
+      setSubmitting(false);
+    })
+  }
 }
 
 function handleDeleteAcivity(id: string) {
-  setActivities([...activities.filter(d => d.id !== id)]);
+  setSubmitting(true);
+  agent.ActivityByAxios.delete(id).then(() => {
+    setActivities([...activities.filter(d => d.id !== id)]);
+    setSubmitting(false);
+  })
+  
 }
 
+if (loading) return (<LoadingComponent content='Locading App...'/>)
   return (
     <>
       <NavBar openForm={handleFormOpen}/>
@@ -60,6 +87,7 @@ function handleDeleteAcivity(id: string) {
           closeForm={handleFormClose}
           createOrEdit={handleCreateOrEditActivity}
           deleteActivity={handleDeleteAcivity}
+          submitting={submitting}
           />
       </Container>
     </>
